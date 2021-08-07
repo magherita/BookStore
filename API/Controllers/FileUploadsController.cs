26090171f8f5b50f91df597b8c48ef7a9175fcaf
Daniel Okello <handsdani@gmail.com>
2021-08-07 16:52:09 +0300

    refactored the file upload logic, pending review

--- API/Controllers/FileUploadsController.cs
+++ API/Controllers/FileUploadsController.cs
@@ -1,54 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Hosting;
 
-namespace API.Controllers
+namespace FileUpload.Controllers
 {
-    public class FileUploadsController : Controller
+    [Route("api/file")]
+    public class FileController : Controller
     {
-        public readonly IHostingEnvironment _hostingEnvironment;
-        public FileUploadsController(IHostingEnvironment _hostingEnvironment)
+        private readonly IWebHostEnvironment _iwebHostEnvironment;
+
+        public FileController(IWebHostEnvironment iwebHostEnvironment)
         {
-            _hostingEnvironment = _hostingEnvironment;
+            _iwebHostEnvironment = iwebHostEnvironment;
         }
 
-        [HttpPost("FileUpload")]
-        public async Task<IActionResult> Index(List<IFormFile> files)
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
         {
-            if (files == null || files.Count == 0)
-                return Content(("file not selected"));
-            long size = files.Sum((f => f.Length));
-            var filePaths = new List<String>();
-
-            foreach (var formFile in files)
+            try
             {
-                if (formFile.Length > 0)
-                {
-                    // Full path to the file in temp location
-
-                    var filePath = Path.Combine(_hostingEnvironment.WebRootPath, "userimages");
-                    filePaths.Add(filePath);
-                    var fileNameWithPath = string.Concat(filePath, "\\", formFile.FileName);
-                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
-                    {
-                        await formFile.CopyToAsync(stream);
-                    }
-                }
+                var path = Path.Combine(_iwebHostEnvironment.WebRootPath, "images", file.FileName);
+                var stream = new FileStream(path, FileMode.Create);
+                await file.CopyToAsync(stream);
+                return  Ok((new { length = file.Length, name = file.FileName }));
             }
-
-            return Ok(new
+            catch 
             {
-                count = files.Count, size, filePaths
-            });
-
+                return BadRequest();
+            }
         }
-        
     }
 }
